c3830a0a1430a958649dccf387161aef3afde459
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-26 15:53:47 +0200

    idk.

--- test.c
+++ test.c
@@ -1,10 +1,13 @@
-static int LKM_init(void)
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init LKM_init(void)
 {
     printk("idk" "\n");
     return 0;
 }
 
-static void LKM_exit(void)
+static void __exit LKM_exit(void)
 {
 }
 
