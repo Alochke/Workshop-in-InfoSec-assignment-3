bee1db098ab0e09fdec0ea7edf0799603eede885
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-26 12:27:12 +0200

    Uploading for checking the size of the required size for the rule table and PAGE_SIZE for compression.

--- rule_table.c
+++ rule_table.c
@@ -1,7 +1,19 @@
 #include "rule_table.h"
 
-int main()
+int __init init()
 {
     printk("%d\n", sizeof(sizeof(rule_t) * MAX_RULES));
     printk("%lu\n", PAGE_SIZE);
+    return 0;
 }
+
+/*
+    Module removal function.
+*/
+static void __exit exit(void)
+{
+    hook_destroy();
+}
+
+module_init(LKM_init);
+module_exit(LKM_exit);
