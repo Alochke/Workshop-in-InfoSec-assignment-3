9bc12be0212b6d96552ba38a37a65b238ff147ba
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-28 17:37:16 +0200

    idk

--- test.c
+++ test.c
@@ -4,19 +4,19 @@
 
 int main()
 {
-    char buf1[sizeof(rule_t)];
-    char buf2[sizeof(rule_t)];
+    char buf1[60];
+    char buf2[60];
 
-    memset(buf1, 'a', sizeof(rule_t) - 1);
-    buf1[sizeof(rule_t) - 1] = 0;
-    memset(buf2, 'b', sizeof(rule_t) - 1);
-    buf2[sizeof(rule_t) - 1] = 0;
+    memset(buf1, 'a', 59);
+    buf1[59] = 0;
+    memset(buf2, 'b', 59);
+    buf2[59] = 0;
 
     int file = fopen("/sys/class/fw/rules/rules", "r+");
 
-    fputs(buf1, sizeof(rule_t), file);
+    fputs(buf1, 60, file);
 
-    fgets(buf2, sizeof(rule_t), file);
+    fgets(buf2, 60, file);
 
     printf("%s\n", buf2);
 
