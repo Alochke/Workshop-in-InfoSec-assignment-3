5ba94cb568cca3da3285dd00ea4da52d71eb26d1
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-28 13:25:59 +0200

    idk.

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main()
 {
     char buf[4];
 
-    FILE *fp = open("/sys/class/fw/rules/rules", "r+");
+    FILE *fp = open("/sys/class/fw/rules/rules", O_RDWR);
 
     fclose(fp);
     return 0;
