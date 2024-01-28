b057149a70dfad6c4ffe3e485cdac6ce07343771
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-01-28 16:19:06 +0200

    idk.

--- test.c
+++ test.c
@@ -4,34 +4,34 @@
 
 
 
+int main()
+{
+    char buf[4];
+
+    FILE* file = fopen("/sys/class/fw/rules/rules", "r+");
+
+    fprintf(file, "idk");
+
+    fgets(buf, 4, file);
+
+    // printf("%s", buf);
+
+    // fclose(file);
+    return 0;
+}
+
 // int main()
 // {
 //     char buf[4];
 
-//     FILE* file = fopen("/sys/class/fw/rules/rules", "r+");
+//     int file = open("/sys/class/fw/rules/rules", O_RDWR);
 
-//     fprintf(file, "idk");
+//     write(file, "idk", 1);
 
 //     // fgets(buf, 4, file);
 
 //     // printf("%s", buf);
 
-//     // fclose(file);
+//     close(file);
 //     return 0;
 // }
-
-int main()
-{
-    char buf[4];
-
-    int file = open("/sys/class/fw/rules/rules", O_RDWR);
-
-    write(file, "idk", 1);
-
-    // fgets(buf, 4, file);
-
-    // printf("%s", buf);
-
-    close(file);
-    return 0;
-}
