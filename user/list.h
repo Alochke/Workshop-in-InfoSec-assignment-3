8066758d62d6ef21b7c525696f1f5a3bcff14104
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-02 14:41:56 +0200

    Checking rule table loading.

--- user/list.h
+++ user/list.h
@@ -2,7 +2,7 @@
 #define LIST
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 typedef struct list_node
 {
 	char *key;
@@ -10,11 +10,13 @@ typedef struct list_node
 	struct list_node *next;
 } list_node;
 
-typedef struct list
+typedef struct
 {
 	unsigned int size;
 	list_node *head;
 } list;
+
+#include "main.h" // This line is here intentionally, otherwise "rule_table_parser.h" would be included without the typedefs it needs to pass pre-processing.
 
 int list_init(list *);
 list_node *list_insert(list *, list_node *);
@@ -22,4 +24,5 @@ list_node *list_insert_key(list *, char *);
 void list_delete(list *, list_node *);
 void list_reverse(list *);
 void list_destroy(list *);
+ 
 #endif
