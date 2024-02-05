7719499c5fffccb3b1f57c57aea8ed138333d8e8
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-05 22:16:53 +0200

    Checking update.

--- module/rule_table.c
+++ module/rule_table.c
@@ -113,16 +113,7 @@ static ssize_t modify(struct device *dev, struct device_attribute *attr, const c
     //Checking the given values are correct.
     for(i = 0; i < count / sizeof(rule_t); i++)
     {
-        if (check_correct(((rule_t*)buf)[i].direction, DIRECTION_VALS, DIRECTION_NUM, INT)
-        || 
-        check_correct(((rule_t*)buf)[i].ack, ACK_VALS, ACK_NUM, INT)
-        ||
-        check_correct(((rule_t*)buf)[i].action, ACTION_VALS, ACTIONS_NUM, INT)
-        ||
-        (((rule_t*)buf)[i].src_prefix_mask != MASK_FROM_SIZE(((rule_t*)buf)[i].src_prefix_size))
-        ||
-        (((rule_t*)buf)[i].dst_prefix_mask != MASK_FROM_SIZE(((rule_t*)buf)[i].dst_prefix_size))
-        )
+        if (check_correct(((rule_t*)buf)[i].direction, DIRECTION_VALS, DIRECTION_NUM, INT))
         {
             return MAIN_FAILURE;
         }
