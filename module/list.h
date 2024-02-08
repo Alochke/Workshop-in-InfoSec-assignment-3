32df7ce840236eabefa1707e22f11c51ade68894
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-08 22:18:43 +0200

    Fixing log update implementation.

--- module/list.h
+++ module/list.h
@@ -10,7 +10,7 @@ typedef struct{
     struct log_row_t* log;
 }log_node;
 
-static inline log_row_t* node_to_log(struct klist_node *node)
+static inline log_row_t* node_to_log(struct klist_node *knode)
 {
-    return ((log_node*)node)->log;
+    return ((log_node*)knode)->log;
 }
