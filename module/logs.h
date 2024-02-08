c08023016f2fe0a9cc676fc4f2dbfe15219eaae4
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-08 15:53:23 +0200

    Fixing log update implementation.

--- module/logs.h
+++ module/logs.h
@@ -11,7 +11,7 @@ int logs_init(void);
 void logs_destroy(void);
 ssize_t logs_read(struct file *filp, char *buff, size_t length, loff_t *offp);
 int logs_open(struct inode *_inode, struct file *_file);
-void logs_update(unsigned char, unsigned char, __be32, __be32, __be16, __be16, reason_t)
+void logs_update(unsigned char protocol, unsigned char action, __be32 src_ip, __be32 dst_ip, __be16 src_port, __be16 dst_port, reason_t reason);
 
 
 #endif
