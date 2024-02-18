252076c73c13683cd76cb691091da195192b2f00
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-18 09:46:52 +0200

    Debugging.

--- user/logs.c
+++ user/logs.c
@@ -47,7 +47,7 @@ int read_logs(int fd)
     rows = malloc(sizeof(log_row_t) * row_num);
     MAIN_MSG_ERR_CHECK(rows == NULL,, MAIN_MALLOC_ERR_MSG);
     MAIN_MSG_ERR_CHECK(read(fd, rows, sizeof(log_row_t) * row_num) != (sizeof(log_row_t) * row_num), free(rows), MAIN_READ_LOGS_ERR_MSG)
-    dprintf(stderr, "check check\n");
+    fprintf(stderr, "check check\n");
 
     printf(LOG_FORMAT_STRING(REASON_FORMAT_STR), "timestamp", "src_ip", "dst_ip", "src_port", "dst_port", "protocol", "action", "reason", "count");
 
