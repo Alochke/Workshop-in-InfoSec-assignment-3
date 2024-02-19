96547ac50ec1934ecaea65e5ab931f4fd011f5aa
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-19 10:28:47 +0200

    testing.

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ from scapy.all import IP, UDP, ICMP, TCP, sendp, Ether, send
 ip_packet = IP(dst = "10.1.1.1")
 
 # Craft the ICMP packet (ping request)
-transort= UDP(sport = 1023)
+transort= UDP(dport = 4)
 
 # Combine the IP and ICMP packets
 packet = ip_packet / transort
