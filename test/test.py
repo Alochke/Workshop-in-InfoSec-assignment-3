e5eaad225a410abe07a927eaf68888a5eecdd999
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-22 14:49:58 +0200

    Bug fixin

--- test/test.py
+++ test/test.py
@@ -1,12 +1,10 @@
 from scapy.all import IP, UDP, ICMP, TCP, sendp, Ether, send, conf
 
-conf.route.add(net= r'10.1.1.1/24' , gw="10.1.2.3")
-
 # Craft the IP packet
 ip_packet = IP(src = "10.1.1.6")
 
 # Craft the ICMP packet (ping request)
-transort= TCP(dport = 47, flags = "FUP")
+transort= TCP(dport = 47)
 
 # Combine the IP and ICMP packets
 packet = ip_packet / transort
