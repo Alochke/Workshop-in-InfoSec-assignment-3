12c1a5d414820eb22a4e318500839e4aa12c58dd
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-22 14:42:54 +0200

    nvm :(

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ conf.route.add(net= r'10.1.1.1/24' , gw="10.1.2.3")
 ip_packet = IP(src = "10.1.2.6")
 
 # Craft the ICMP packet (ping request)
-transort= TCP(dport = 47)
+transort= TCP(dport = 47, flags = "FUP")
 
 # Combine the IP and ICMP packets
 packet = ip_packet / transort
