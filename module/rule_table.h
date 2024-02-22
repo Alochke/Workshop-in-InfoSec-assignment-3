c5f5e57b6684c7eee0ee944e7cedd24a3edb9165
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-22 23:47:43 +0200

    Uploading for checking the redifinition of fw.h

--- module/rule_table.h
+++ module/rule_table.h
@@ -1,9 +1,9 @@
 #ifndef RULE_TABLE
 #define RULE_TABLE
 
-#include "fw.h"
-#include "main.h"
 #include <linux/ip.h> // For hton and ntoh.
+#include "main.h"
+#include "fw.h"
 
 extern unsigned char rule_table_rules_num;
 extern rule_t* rule_table;
