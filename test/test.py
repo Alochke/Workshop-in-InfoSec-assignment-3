63e68ff1475ce0a4c0a38d5958670f2bc180b768
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-22 14:43:23 +0200

    nvm :(

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from scapy.all import IP, UDP, ICMP, TCP, sendp, Ether, send, conf
 conf.route.add(net= r'10.1.1.1/24' , gw="10.1.2.3")
 
 # Craft the IP packet
-ip_packet = IP(src = "10.1.2.6")
+ip_packet = IP(src = "10.1.1.6")
 
 # Craft the ICMP packet (ping request)
 transort= TCP(dport = 47, flags = "FUP")
