2391d41ad90d1b2e0ca9f6908cd053bff6ceaded
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-02-25 14:35:08 +0200

    So fucking close.

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 from scapy.all import IP, UDP, ICMP, TCP, sendp, Ether, send, conf
 
 # Craft the IP packet
-ip_packet = IP(dst = "10.1.2.15", proto = 7)
+ip_packet = IP(dst = "10.1.2.15")
 
 # Craft the ICMP packet (ping request)
 transort= TCP(sport = 1024, dport = 43534)
