3c9ba8cad4bfc3b1a6f4de67db4388e1efa325ce
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-03-24 17:30:39 +0200

    work

--- module/connection_table_list.h
+++ module/connection_table_list.h
@@ -10,7 +10,7 @@ void list_get(struct klist_node*);
 /*
     Returns a pointer to the connection_table_entry that is also a member of the node that knode is a member of.
 */
-static inline connection_table_entry* node_to_log(struct klist_node *knode)
+static inline connection_table_entry* node_to_connection_table_entry(struct klist_node *knode)
 {
     return (connection_table_entry*) (((node*)knode)->content);
 }
