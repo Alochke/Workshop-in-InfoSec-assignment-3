6ead0d91cfc451026d5067e9a91caa0f6259e255
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-03-24 17:15:55 +0200

    work

--- module/logs_list.c
+++ module/logs_list.c
@@ -5,7 +5,7 @@
     This function is called each time a node is added to the list, and in our use-case every time klist_add_tail is called on a node, with node as klist_add_tail's parameter.
 
     Parameters:
-    - node: The node that is being added to a list.
+    - n: The node that is being added to a list.
 */
 void list_get(struct klist_node* n)
 {
